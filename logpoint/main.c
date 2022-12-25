03c0c9af4963556b27a0741f03d67ea26bf2b4e7
Rob Hoelz <rob@hoelz.ro>
2022-12-25 07:57:13 -0600

    Run work in traced child

--- logpoint/main.c
+++ logpoint/main.c
@@ -1,4 +1,13 @@
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define die(msg, args...)\
+    fprintf(stderr, msg "\n", ##args);\
+    exit(1);
 
 extern long long fib(int n);
 
@@ -25,7 +34,34 @@ foo()
 int
 main(void)
 {
+    pid_t tracee;
+    int status;
+
     breakpoint_line_no = 0;
-    foo();
+
+    tracee = fork();
+    if(tracee == -1) {
+        die("unable to spawn tracee process: %s", strerror(errno));
+    }
+
+    if(tracee) {
+        int exit_status;
+
+        // XXX if SIGTRAP is raised, we need to pause the offending thread, *plus* inform
+        //     the main thread that the offending thread was stopped
+        printf("waiting on tracee child...\n");
+        status = waitpid(tracee, &exit_status, 0);
+        if(status == -1) {
+            fprintf(stderr, "failed to wait for tracee: %s\n", strerror(errno));
+        } else {
+            printf("exit status for tracee: %d\n", exit_status);
+        }
+    } else {
+        // XXX set me up as a tracee
+        foo();
+        return 0;
+    }
+
+    // XXX pause the thread upon break, then print the target symbol, then resume the thread
     return 0;
 }
