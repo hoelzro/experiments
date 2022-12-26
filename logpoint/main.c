f0415fcb5ce6376f20ab9fc8777609a16eb3bf2c
Rob Hoelz <rob@hoelz.ro>
2022-12-26 08:20:11 -0600

    Set up tracee as actual tracee and implement trace handler loop skeleton

--- logpoint/main.c
+++ logpoint/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ptrace.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
@@ -49,19 +50,47 @@ main(int argc, char **argv)
     }
 
     if(tracee) {
-        int exit_status;
+        while(1) {
+            int exit_status;
 
-        // XXX if SIGTRAP is raised, we need to pause the offending thread, *plus* inform
-        //     the main thread that the offending thread was stopped
-        printf("waiting on tracee child...\n");
-        status = waitpid(tracee, &exit_status, 0);
-        if(status == -1) {
-            fprintf(stderr, "failed to wait for tracee: %s\n", strerror(errno));
-        } else {
-            printf("exit status for tracee: %d\n", exit_status);
+            status = waitpid(tracee, &exit_status, 0);
+            if(status == -1) {
+                fprintf(stderr, "failed to wait for tracee: %s\n", strerror(errno));
+                break;
+            } else {
+                if(!WIFSTOPPED(exit_status)) {
+                    if(WIFEXITED(exit_status)) {
+                        printf("exit status for tracee: %d\n", WEXITSTATUS(exit_status));
+                    } else if(WIFSIGNALED(exit_status)) {
+                        printf("exit signal for tracee: %d\n", WTERMSIG(exit_status));
+                    } else {
+                        printf("some other trace event =S (0x%x)\n", exit_status);
+                    }
+
+                    break;
+                }
+
+                if(WSTOPSIG(exit_status) == SIGTRAP) {
+                    printf("tracee hit a breakpoint\n");
+                    status = ptrace(PTRACE_CONT, tracee, 0, 0);
+                } else {
+                    status = ptrace(PTRACE_CONT, tracee, 0, WSTOPSIG(exit_status));
+                }
+
+                if(status == -1) {
+                    fprintf(stderr, "Unable to resume tracee: %s\n", strerror(errno));
+                    break;
+                }
+            }
         }
     } else {
-        // XXX set me up as a tracee
+        status = ptrace(PTRACE_TRACEME, 0, 0, 0);
+        if(status == -1) {
+            die("unable to set tracee up for tracing: %s", strerror(errno));
+        }
+
+        // XXX set options so we die if our parent does?
+
         foo();
         return 0;
     }
