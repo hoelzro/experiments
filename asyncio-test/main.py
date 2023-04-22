f5fe1049765152e63798f1c7006ec60645a878aa
Rob Hoelz <rob@hoelz.ro>
2023-04-22 08:36:32 -0500

    Try subprocesses with asyncio

--- asyncio-test/main.py
+++ asyncio-test/main.py
@@ -4,14 +4,18 @@ import random
 async def sleepy_time(sleeper_id):
     t = random.random() * 10
     print(f'[{sleeper_id}] sleeping for {t} seconds')
-    await asyncio.sleep(t)
+    status_cmd = random.choice(['true', 'false'])
+    proc = await asyncio.create_subprocess_shell(f'sleep {t} ; {status_cmd}')
+    exit_code = await proc.wait()
     print(f'[{sleeper_id}] done sleeping')
+    return exit_code == 0
 
 async def main():
     sleepers = [ sleepy_time(i) for i in range(5) ]
 
     print('waiting for sleepers')
-    await asyncio.gather(*sleepers)
+    res = await asyncio.gather(*sleepers)
     print('done waiting')
+    print(res)
 
 asyncio.run(main())
