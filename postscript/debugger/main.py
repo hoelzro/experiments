62a486e31e092c464b2f216c592423c6d8211d7e
Rob Hoelz <rob@hoelz.ro>
2024-02-15 21:20:11 -0600

    Start a simple textual wrapper around the interpreter
    It's got a ton of bugs, but this is a start

--- postscript/debugger/main.py
+++ postscript/debugger/main.py
@@ -1,10 +1,17 @@
 from collections import deque, ChainMap
 from dataclasses import dataclass
 import inspect
+import io
+import sys
 
 from typing import Optional, Self
 
 from pprint import pprint
+from textual.app import App
+from textual.binding import Binding
+from textual.containers import Horizontal, Vertical
+from textual.widget import Widget
+from textual.widgets import Footer, Log, Static, TextArea
 
 class Frame:
     '''
@@ -318,9 +325,66 @@ class Interpreter:
             self.execution_stack.pop()
         assert not self._is_building_executable_array() # XXX right?
 
+class SourceCode(Widget):
+    def __init__(self, text, **kwargs):
+        super().__init__(**kwargs)
+        self.text = text
+        self.highlight_pos = None
+
+    def render(self):
+        lines = self.text.splitlines()
+        if self.highlight_pos is not None:
+            line_no, col_start, col_end = self.highlight_pos
+            line = lines[line_no - 1]
+            lines[line_no - 1] = ''.join([
+                line[:col_start-1],
+                '[r]',
+                line[col_start-1:col_end],
+                '[/r]',
+                line[col_end:],
+            ])
+
+        return '\n'.join(lines)
+
+    def highlight(self, line, col_start, col_end):
+        self.highlight_pos = (line, col_start, col_end)
+
+class DebuggerApp(App):
+    CSS_PATH = 'layout.tcss'
+
+    BINDINGS = [
+        ('enter', 'step()', 'Step'),
+        ('q', 'quit()', 'Quit'),
+    ]
+
+    def compose(self):
+        # XXX is this the right place to put this?
+        self.interp = Interpreter()
+
+        with open(sys.argv[1], 'r') as f:
+            source_code = f.read()
+        self.stepper = self.interp.execute(Scanner(io.StringIO(source_code)))
+
+        self.log_widget = Log(classes='output_pane')
+
+        with Vertical():
+            with Horizontal():
+                self.src = SourceCode(source_code, classes='box')
+                yield self.src
+                yield Static('Stack Pane', classes='box')
+            yield self.log_widget
+        yield Footer()
+
+    def action_quit(self):
+        self.exit()
+
+    def action_step(self):
+        next_word = next(self.stepper)
+        assert next_word.line is not None, repr(next_word)
+        self.src.highlight(line=next_word.line, col_start=next_word.column, col_end=next_word.column+next_word.length)
+        self.src.refresh()
+        self.log_widget.write_line(str(next_word) + ' ' + str(self.src.highlight_pos))
+
 if __name__ == '__main__':
-    import sys
-    with open(sys.argv[1], 'r') as f:
-        i = Interpreter()
-        for _ in i.execute(Scanner(f)):
-            print('got control back')
+    app = DebuggerApp()
+    app.run()
